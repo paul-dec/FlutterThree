13967e6c12c3cb4776e1d9ad8043da16355b22d0
Taotao <yuantao.li@epitech.eu>
2021-12-09 23:05:35 +0100

    add firebase access

--- adminapp/lib/pages/main_page.dart
+++ adminapp/lib/pages/main_page.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -24,9 +25,19 @@ class _MainPageState extends State<MainPage> {
         appBar: AppBar(
           title: Text('Hello ${_currentUser.email}'),
         ),
-        body: const Center(
-          child: Text("Bonjour"),
-        )
+        body: StreamBuilder<DocumentSnapshot>(
+          stream: FirebaseFirestore.instance.collection('users').doc(_currentUser.uid).snapshots(),
+          builder: (BuildContext context,AsyncSnapshot<DocumentSnapshot> snapshot) {
+            if (snapshot.hasError) {
+              return Text('Error: ${snapshot.error}');
+            }
+            switch(snapshot.connectionState) {
+              case ConnectionState.waiting: return Text('Loading...');
+              default:
+                return Text(snapshot.data?['name']);
+            }
+            },
+        ),
     );
   }
 }
